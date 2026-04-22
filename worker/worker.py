49db80fc1248c70e465e683bfebfedc0be151de4
Marline Khavele <khavelemarline@gmail.com>
2026-04-22 22:07:41 +0200

    fix: remove redis hardcoded url on worker

--- worker/worker.py
+++ worker/worker.py
@@ -1,9 +1,12 @@
 import redis
 import time
 import os
-import signal
 
-r = redis.Redis(host="localhost", port=6379)
+
+REDIS_HOST = os.getenv("REDIS_HOST", "redis")
+REDIS_PORT = int(os.getenv("REDIS_PORT", 6379))
+
+r = redis.Redis(host=REDIS_HOST, port=REDIS_PORT, decode_responses=True)
 
 def process_job(job_id):
     print(f"Processing job {job_id}")
